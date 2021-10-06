e19ff318fe87e66bd54f3591adf6b6fd6dcf4ea6
Amph <--global>
2021-10-06 19:44:32 +0200

    Los fuegos arden

--- Practica 2/Entrega 2/skeleton/Firework.cpp
+++ Practica 2/Entrega 2/skeleton/Firework.cpp
@@ -4,6 +4,7 @@ Firework::Firework(Vector3 pos, Vector3 speed, Vector3 accel, float drag, float 
 	Particle(pos,speed,accel,drag,mass,rd,col,limit), mType_(type), count(mCount)
 {
 	mFireWorks_ = std::vector<mFireworkRules>(mCount);
+	initFireWorksRules();
 }
 
 Firework::~Firework()
@@ -19,6 +20,22 @@ void Firework::integrate(float t)
 
 }
 
+void Firework::initFireWorksRules()
+{
+	for (mFireworkRules r : mFireWorks_) {
+		
+	}
+}
+
 void Firework::OnDestroy()
 {
+}
+
+mFireworkRules::mFireworkRules(Type mType, float minAge, float maxAge, Vector3 minVelocity, Vector3 maxVelocity, float damp)
+{
+}
+
+void mFireworkRules::create(Firework* mFirework, Firework* parent= nullptr)
+{
+
 }
--- Practica 2/Entrega 2/skeleton/Firework.h
+++ Practica 2/Entrega 2/skeleton/Firework.h
@@ -4,12 +4,25 @@
 
 enum Type{ Basico, Simple, Multiple}; //basico no explota en más, Simple genera fuegos simples, multiples genera simples
 
-struct mFireworkRules
+struct Payload {
+    Type mType_;
+    unsigned int mCount_;
+    Payload(Type mType, int count): mType_(mType), mCount_(count){}
+};
+class Firework; // declaracion previa para el create
+class mFireworkRules
 {
-    Type mType;
-    unsigned int mCount;
+protected:
+    Type mType_;
+    float minAge, maxAge;
+    Vector3 minVelocity_, maxVelocity_;
+    float damp_;
+    std::vector<Payload> mPayloads_;
+public:
+    mFireworkRules(){}
+    mFireworkRules(Type mType, float minAge, float maxAge, Vector3 minVelocity, Vector3 maxVelocity, float damp);
+    void create(Firework* mFirework, Firework* parent );
 
-    mFireworkRules(Type t, int c): mType(t), mCount(c){}
 };
 
 
@@ -19,13 +32,16 @@ class Firework :
 public:
     Firework(Vector3 pos, Vector3 speed, Vector3 accel, float drag, float mass, float rd, Vector4 col, double limit, Type type, int mCount);
     virtual ~Firework();
+
     virtual void integrate(float t);
 
 private:
+    void initFireWorksRules();
     void OnDestroy();
     
     std::vector<mFireworkRules> mFireWorks_;
     int count;
     Type mType_;
+
 };
 
