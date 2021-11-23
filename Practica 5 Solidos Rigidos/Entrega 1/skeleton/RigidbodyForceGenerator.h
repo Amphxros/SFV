b4a6e9ee35de1aabab3b7071c28901d899ddf580
Amph <--global>
2021-11-23 16:25:30 +0100

    registro completo a falta de añadirlo a la escena

--- Practica 5 Solidos Rigidos/Entrega 1/skeleton/RigidbodyForceGenerator.h
+++ Practica 5 Solidos Rigidos/Entrega 1/skeleton/RigidbodyForceGenerator.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <PxPhysicsAPI.h>
-
+#include "Rigidbody.h"
 class RigidbodyForceGenerator
 {
 	public:
 		RigidbodyForceGenerator() {}
-		virtual void integrateForce(physx::PxRigidDynamic* rb, float t) = 0;
+		virtual void integrateForce(RigidBody* rb, float t) = 0;
 };
 
--- Practica 5 Solidos Rigidos/Entrega 1/skeleton/RigidbodyForceRegistry.cpp
+++ Practica 5 Solidos Rigidos/Entrega 1/skeleton/RigidbodyForceRegistry.cpp
@@ -6,24 +6,50 @@ RigidbodyForceRegistry::RigidbodyForceRegistry()
 
 RigidbodyForceRegistry::~RigidbodyForceRegistry()
 {
+	clear();
 }
 
 void RigidbodyForceRegistry::add(RigidBody* rb, RigidbodyForceGenerator* force)
 {
+	RigidBodyForcePair pair;
+	pair.rb = rb;
+	pair.force = force;
+
+	mRegistry_.push_back(pair);
 }
 
 void RigidbodyForceRegistry::remove(RigidBody* rb, RigidbodyForceGenerator* force)
 {
+	auto it = mRegistry_.begin();
+
+	while (it != mRegistry_.end()) {
+		if ((*it).rb == rb && (*it).force== force) {
+			it = mRegistry_.erase(it);
+		}
+		else ++it;
+	}
 }
 
 void RigidbodyForceRegistry::remove(RigidBodyForcePair registry)
 {
+	auto it = mRegistry_.begin();
+
+	while (it != mRegistry_.end()) {
+		if ((*it).rb == registry.rb && (*it).force == registry.force) {
+			it = mRegistry_.erase(it);
+		}
+		else ++it;
+	}
 }
 
 void RigidbodyForceRegistry::clear()
 {
+	mRegistry_.clear();
 }
 
 void RigidbodyForceRegistry::integrateForces(float t)
 {
+	for (RigidBodyForcePair pair : mRegistry_) {
+		pair.force->integrateForce(pair.rb, t);
+	}
 }
